13c064a9398dc176f93f91188a800e0d4578b317
Riza Fahmi <rizafahmi@gmail.com>
2019-01-08 12:55:34 +0700

    Remove rel from a href

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,19 +6,13 @@ const Footer = () => (
     <div className="text-center max-w-xl mx-auto p-4 md:p-8 text-sm">
       <ul className="list-reset inline-flex">
         <li className="mx-4">
-          <a href="/" rel="noopener noreferrer">
-            Home
-          </a>
+          <a href="/">Home</a>
         </li>
         <li className="mx-4">
-          <a href="/" rel="noopener noreferrer">
-            About
-          </a>
+          <a href="/">About</a>
         </li>
         <li className="mx-4">
-          <a href="/articles" rel="noopener noreferrer">
-            Articles
-          </a>
+          <a href="/articles">Articles</a>
         </li>
         <li className="mx-4">
           <a
