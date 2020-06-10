d87bbad6abdbaf7e4ca2d28f79102213c1641358
Riza Fahmi <rizafahmi@gmail.com>
2020-06-11 05:38:30 +0700

    Add short url for bwa svelte courses

---
+++ src/pages/BWAFUNDING.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class BWAFUNDING extends React.Component {
+  render() {
+    return (
+      <Helmet>
+        <meta
+          http-equiv="refresh"
+          content={`0;url=https://docs.google.com/document/d/1tZhCvbubDDSODml6vm-ZoV-6yYfqXb1caQe4ZrBPwfc/edit?usp=sharing`}
+        />
+      </Helmet>
+    );
+  }
+}
+
+export default BWAFUNDING;
---
+++ src/pages/BWASVELTE.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class BWASVELTE extends React.Component {
+  render() {
+    return (
+      <Helmet>
+        <meta
+          http-equiv="refresh"
+          content={`0;url=https://www.buildwithangga.com/kelas/svelte-javascript-web-development`}
+        />
+      </Helmet>
+    );
+  }
+}
+
+export default BWASVELTE;
