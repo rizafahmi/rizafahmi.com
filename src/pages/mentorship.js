88d39bf20dca421ed90eb7d2843db778d29d87c7
Riza Fahmi <rizafahmi@gmail.com>
2020-11-08 15:19:00 +0700

    Add more topic

--- src/pages/mentorship.js
+++ src/pages/mentorship.js
@@ -94,6 +94,7 @@ const Mentorship = () => {
             <li>Developer tools, Vim, Git, zsh, etc</li>
             <li>Scaling</li>
             <li>Arsitektur</li>
+            <li>Dan lain-lain</li>
           </ul>
           <h3>Catatan</h3>
           <p>
