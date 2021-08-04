7a4feb67796ec0f430717947ece4877dff217486
Riza Fahmi <rizafahmi@gmail.com>
2021-08-04 21:52:08 +0700

    Add rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
   return {
     dir: { input: 'src', output: 'dist' },
