7ed3fec9d80e17c20a2161a0ec77f8bedd3c62ef
Riza Fahmi <rizafahmi@gmail.com>
2021-10-08 10:13:23 +0700

    Add simple course platform feature

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,22 @@
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
 
+  eleventyConfig.addFilter('filterCourse', function (courses, url) {
+    const [_, _course, slug] = url.split('/');
+    const currentCourse = courses.find(function (course) {
+      return course.slug === slug;
+    });
+    return currentCourse;
+  });
+
   return {
     dir: { input: 'src', output: 'dist' },
     dataTemplate: 'njk',
-    markdownTemplateEngine: 'njk'
+    markdownTemplateEngine: 'njk',
   };
 };
