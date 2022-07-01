992d323c54cd37752df1de5a4c3ad92d71080b28
Riza Fahmi <rizafahmi@gmail.com>
2022-07-01 00:39:07 +0000

    Add redirection

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('src/_redirects');
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
 
