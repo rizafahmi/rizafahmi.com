9db240e3785329bf32899d144d68abb24e5fb2d6
Riza Fahmi <rizafahmi@gmail.com>
2024-03-26 11:30:32 +0700

    Fixing feed xml

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,10 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('src/_redirects');
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginRss);
 
   return {
     dir: { input: 'src', output: 'dist' },
